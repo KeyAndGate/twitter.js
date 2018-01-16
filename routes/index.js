dc0cf48588a119e78f414b1cce146139becdc31e
Whitman Johnson <whit.ross.j@gmail.com>
2018-01-16 12:40:01 -0500

    form working and dynamically prepopulating

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,30 @@ const express = require('express');
 const router = express.Router();
 const tweetBank = require('../tweetBank');
 
+const bodyParser = require('body-parser');
+
+const urlEncodedParser = bodyParser.urlencoded({ extended: false });
+
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+    res.render( 'index', { tweets: tweets, showForm: true } );
+});
+
+router.post('/tweets', urlEncodedParser, function (req, res) {
+    if(!req.body) return res.sendStatus(400);
+    tweetBank.add(req.body.name, req.body.text);
+    res.redirect('/');
 });
 
 router.get('/users/:name', (req, res) => {
     const user = {name: req.params.name};
     const userTweets = tweetBank.find(user);
-    res.render('index', {tweets: userTweets} );
+    res.render('index', {tweets: userTweets, showForm: true, pageName: user.name} );
 });
 
 router.get('/tweets/:id', (req, res) => {
   const tweetId = {id: req.params.id};
-  console.log('tweetId', tweetId);
   const tweet = tweetBank.find(tweetId);
-  console.log('tweet', tweet);
   res.render('index', {tweets: tweet});
 });
 module.exports = router;
