9b0a51672a2e0a23a64f66e71d23af132337f8c2
Gabriel Messager <gabriel@frame.io>
2018-01-16 11:41:04 -0500

    wip routing

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-// could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
+});
+
+router.get('/users/:name', (req, res) => {
+    const user = {name: req.params.name};
+    const userTweet = tweetBank.find(user);
+    res.render(userTweet);
 });
 
 module.exports = router;
