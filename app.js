adafaefc27785755513405467c1916fe0697b4bf
Gabriel Messager <gabriel@frame.io>
2018-01-16 11:20:38 -0500

    wip step 7

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require( 'express' );
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
 const app = express();
 
 const people = [{name: 'James'}, {name: 'Harry'}, {name: 'John'}];
@@ -11,21 +12,25 @@ const data = {
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
+app.use(morgan('combined'));
 nunjucks.configure('views', { noCache: true });
 
-app.use(morgan('combined'));
+app.use('/', routes);
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url} ${res.statusCode}`);
-    next();
+app.get('/stylesheets/style.css', (req, res) => {
+  res.render('/public/stylesheets/style.css');
 });
-
-app.use('/special', (req, res, next) => {
-    console.log('special access');
-    next();
-});
-
-app.get('/', (req, res) => res.render('index', data))
+// app.use((req, res, next) => {
+//     console.log(`${req.method} ${req.url} ${res.statusCode}`);
+//     next();
+// });
+//
+// app.use('/special', (req, res, next) => {
+//     console.log('special access');
+//     next();
+// });
+//
+// app.get('/', (req, res) => res.render('index', data))
 
 app.listen(3000, function(){
     console.log('server running');
