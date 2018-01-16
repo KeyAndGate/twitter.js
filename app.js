791bba92fe88af15326b69b0b531fa57f06d819c
Gabriel Messager <gabriel@frame.io>
2018-01-16 13:06:34 -0500

    adds socketio

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require( 'express' );
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
+const socketio = require('socket.io');
 const app = express();
 
 const people = [{name: 'James'}, {name: 'Harry'}, {name: 'John'}];
@@ -14,9 +15,8 @@ app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 app.use(morgan('combined'));
 app.use(express.static('public'));
-app.use('/', routes);
 nunjucks.configure('views', { noCache: true });
 
-app.listen(3000, function(){
-    console.log('server running');
-});
+const server = app.listen(3000, () => console.log('server running'));
+const io = socketio.listen(server);
+app.use('/', routes(io));
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,35 @@
-const express = require('express');
-const router = express.Router();
-const tweetBank = require('../tweetBank');
+module.exports = function(io) {
+  const express = require('express');
+  const router = express.Router();
+  const tweetBank = require('../tweetBank');
 
-const bodyParser = require('body-parser');
+  const bodyParser = require('body-parser');
 
-const urlEncodedParser = bodyParser.urlencoded({ extended: false });
+  const urlEncodedParser = bodyParser.urlencoded({ extended: false });
 
-router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
-    res.render( 'index', { tweets: tweets, showForm: true } );
-});
+  router.get('/', function (req, res) {
+    let tweets = tweetBank.list();
+      res.render( 'index', { tweets: tweets, showForm: true } );
+  });
 
-router.post('/tweets', urlEncodedParser, function (req, res) {
-    if(!req.body) return res.sendStatus(400);
-    tweetBank.add(req.body.name, req.body.text);
-    res.redirect('/');
-});
+  router.post('/tweets', urlEncodedParser, function (req, res) {
+      if(!req.body) return res.sendStatus(400);
+      tweetBank.add(req.body.name, req.body.text);
+      io.sockets.emit('newTweet', {text: 'dummy text'});
+      // res.redirect('/');
+  });
 
-router.get('/users/:name', (req, res) => {
-    const user = {name: req.params.name};
-    const userTweets = tweetBank.find(user);
-    res.render('index', {tweets: userTweets, showForm: true, pageName: user.name} );
-});
+  router.get('/users/:name', (req, res) => {
+      const user = {name: req.params.name};
+      const userTweets = tweetBank.find(user);
+      res.render('index', {tweets: userTweets, showForm: true, pageName: user.name} );
+  });
 
-router.get('/tweets/:id', (req, res) => {
-  const tweetId = {id: req.params.id};
-  const tweet = tweetBank.find(tweetId);
-  res.render('index', {tweets: tweet});
-});
-module.exports = router;
+  router.get('/tweets/:id', (req, res) => {
+    const tweetId = {id: req.params.id};
+    const tweet = tweetBank.find(tweetId);
+    res.render('index', {tweets: tweet});
+  });
+
+  return router;
+}
